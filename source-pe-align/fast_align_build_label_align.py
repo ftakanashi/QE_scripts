5f8a92c1aa50fc7706b00e5a5dce37f515e5858b
wyzypa <wyzypa@gmail.com>
2020-10-02 19:39:41 +0900

    20201002-6

--- source-pe-align/fast_align_build_label_align.py
+++ source-pe-align/fast_align_build_label_align.py
@@ -43,7 +43,7 @@ def main():
         os.makedirs(args.tmp_working_dir)
 
     concat_corpus = os.path.join(args.tmp_working_dir, 'concat_corpus')
-    cmd = "paste " + str(args.src) + " " + str(args.tgt) + " > awk -F '\\t' '{print $1 \" ||| \" $2}' > " + \
+    cmd = "paste " + str(args.src) + " " + str(args.tgt) + " | awk -F '\\t' '{print $1 \" ||| \" $2}' > " + \
           concat_corpus
     run_cmd(cmd)
 
