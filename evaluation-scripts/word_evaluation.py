d904c6cafc5178b7c2db989419c4420e891fdfbf
wyzypa <wyzypa@gmail.com>
2021-01-23 23:07:48 +0900

    20210123-2

--- evaluation-scripts/word_evaluation.py
+++ evaluation-scripts/word_evaluation.py
@@ -54,13 +54,23 @@ def compute_scores(refe_fn, pred_fn, tag_opts):
         recall = recall_score(num_ref_tags, num_pred_tags)
         # f1 = f1_score(num_ref_tags, num_pred_tags, average='weighted', pos_label=None)
         if precision + recall == 0:
-            f1 = 0
+            f1 = 0.0
         else:
             f1 = 2 * (precision * recall) / (precision + recall)
         res[tag] = {'precision': precision, 'recall': recall, 'f1': f1}
-    # res['total_f1'] = f1_score(ref_tags, pred_tags, average='weighted', pos_label=None)
 
-    return res
+    tag_map = {}
+    tag_i = 0
+    for ref_tag in ref_tags:
+        if ref_tag not in tag_map:
+            tag_map[ref_tag] = tag_i
+            tag_i += 1
+
+    num_ref_tags = [tag_map[t] for t in ref_tags]
+    num_pred_tags = [tag_map.get(t, tag_i) for t in pred_tags]
+    total_f1 = f1_score(num_ref_tags, num_pred_tags, average='weighted', pos_label=None)
+
+    return res, total_f1
 
 def main():
     args = parse_args()
@@ -70,15 +80,15 @@ def main():
         refe_fn = f'{args.reference_prefix}{type}'
         pred_fn = f'{args.prediction_prefix}{type}'
         tag_opts = TAG_OPTIONS[args.mode]
-        res = compute_scores(refe_fn, pred_fn, tag_opts)
+        res, total_f1 = compute_scores(refe_fn, pred_fn, tag_opts)
 
         print(f'======== {type}(P/R/F1) =========')
         for tag in res:
             info = res[tag]
             print('{}: {:.4} / {:.4} / {:.4}'.format(
-                tag, info['precision'], info['recall'], float(info['f1'])
+                tag, info['precision'], info['recall'], info['f1']
             ))
-        # print('TOTAL F1: {}'.format(res['total_f1']))
+        print('TOTAL F1: {:.4}'.format(total_f1))
         print('')
 
 if __name__ == '__main__':
