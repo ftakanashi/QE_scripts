bce4b542029b1db0192a7f6a41376ab2c4b6edf0
wyzypa <wyzypa@gmail.com>
2021-01-23 16:20:11 +0900

    20210123

---
+++ evaluation-scripts/word_evaluation.old.py
@@ -0,0 +1,149 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+import argparse
+import numpy as np
+import os
+
+from pathlib import Path
+from sklearn.metrics import f1_score, recall_score, precision_score
+
+
+def list_of_lists(a_list):
+    '''
+    check if <a_list> is a list of lists
+    '''
+    if isinstance(a_list, (list, tuple, np.ndarray)) and \
+            len(a_list) > 0 and \
+            all([isinstance(l, (list, tuple, np.ndarray)) for l in a_list]):
+        return True
+    return False
+
+
+def flatten(lofl):
+    '''
+    convert list of lists into a flat list
+    '''
+    if list_of_lists(lofl):
+        return [item for sublist in lofl for item in sublist]
+    elif type(lofl) == dict:
+        return lofl.values()
+
+
+def compute_scores(true_tags, test_tags):
+    flat_true = flatten(true_tags)
+    flat_pred = flatten(test_tags)
+    rev_flat_true = [1 if t == 0 else 0 for t in flat_true]
+    rev_flat_pred = [1 if t == 0 else 0 for t in flat_pred]
+
+    ok_preceision = precision_score(flat_true, flat_pred)
+    ok_recall = recall_score(flat_true, flat_pred)
+    bad_precision = precision_score(rev_flat_true, rev_flat_pred)
+    bad_recall = recall_score(rev_flat_true, rev_flat_pred)
+
+    bad_f1, ok_f1 = f1_score(flat_true, flat_pred, average=None, pos_label=None)
+
+    # Matthews correlation coefficient (MCC)
+    # true/false positives/negatives
+    tp = tn = fp = fn = 0
+    for pred_tag, gold_tag in zip(flat_pred, flat_true):
+        if pred_tag == 1:
+            if pred_tag == gold_tag:
+                tp += 1
+            else:
+                fp += 1
+        else:
+            if pred_tag == gold_tag:
+                tn += 1
+            else:
+                fn += 1
+
+    mcc_numerator = (tp * tn) - (fp * fn)
+    mcc_denominator = ((tp + fp) * (tp + fn) * (tn + fp) * (tn + fn)) ** 0.5
+    mcc = mcc_numerator / (mcc_denominator + 1e-5)
+
+    return ok_preceision, ok_recall, ok_f1, bad_precision, bad_recall, bad_f1, mcc
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('-rst', '--ref_source_tags', type=Path)
+    parser.add_argument('-rt', '--ref_tags', type=Path, default=None)
+    parser.add_argument('-rgt', '--ref_gap_tags', type=Path, default=None)
+
+    parser.add_argument('-pst', '--pred_source_tags', type=Path)
+    parser.add_argument('-pt', '--pred_tags', type=Path, default=None)
+    parser.add_argument('-pgt', '--pred_gap_tags', type=Path, default=None)
+
+    args = parser.parse_args()
+
+    return args
+
+
+def main():
+    args = parse_args()
+
+    TAG_MAP = {
+        'OK': 1,
+        'BAD': 0
+    }
+
+    def read_tag(fn):
+        with Path(fn).open() as f:
+            return [[TAG_MAP[t] for t in l.strip().split()] for l in f]
+
+    # Source
+    ref_source_tags = read_tag(args.ref_source_tags)
+    pred_source_tags = read_tag(args.pred_source_tags)
+    src_ok_pre, src_ok_rec, src_ok_f1, src_bad_pre, src_bad_rec, src_bad_f1, src_mcc = compute_scores(ref_source_tags,
+                                                                                                      pred_source_tags)
+    # f1_bad_src, f1_good_src, mcc_src = compute_scores(ref_source_tags, pred_source_tags)
+    src_f1_multi = src_ok_f1 * src_bad_f1
+
+    print(f'src_ok_precision: {src_ok_pre:.4}')
+    print(f'src_ok_recall: {src_ok_rec:.4}')
+    print(f'src_ok_f1: {src_ok_f1:.4}')
+    print(f'src_bad_precision: {src_bad_pre:.4}')
+    print(f'src_bad_recall: {src_bad_rec:.4}')
+    print(f'src_bad_f1: {src_bad_f1:.4}')
+    print(f'src_mcc:{src_mcc:.4}')
+    print('---')
+
+    if args.ref_tags is not None and args.pred_tags is not None:
+        # MT (only word or word&gap)
+        ref_mt_tags = read_tag(args.ref_tags)
+        pred_mt_tags = read_tag(args.pred_tags)
+        mt_ok_pre, mt_ok_rec, mt_ok_f1, mt_bad_pre, mt_bad_rec, mt_bad_f1, mt_mcc = compute_scores(ref_mt_tags,
+                                                                                                   pred_mt_tags)
+        mt_f1_multi = mt_ok_f1 * mt_bad_f1
+
+        print(f'mt_ok_precision: {mt_ok_pre:.4}')
+        print(f'mt_ok_recall: {mt_ok_rec:.4}')
+        print(f'mt_ok_f1: {mt_ok_f1:.4}')
+        print(f'mt_bad_precision: {mt_bad_pre:.4}')
+        print(f'mt_bad_recall: {mt_bad_rec:.4}')
+        print(f'mt_bad_f1: {mt_bad_f1:.4}')
+        print(f'mt_mcc:{mt_mcc:.4}')
+        print('---')
+
+    # GAP
+    if args.ref_gap_tags is not None and args.pred_gap_tags is not None:
+        ref_gap_tags = read_tag(args.ref_gap_tags)
+        pred_gap_tags = read_tag(args.pred_gap_tags)
+        gap_ok_pre, gap_ok_rec, gap_ok_f1, gap_bad_pre, gap_bad_rec, gap_bad_f1, gap_mcc = compute_scores(ref_gap_tags,
+                                                                                                          pred_gap_tags)
+        gap_f1_multi = gap_ok_f1 * gap_bad_f1
+
+        print(f'gap_ok_precision: {gap_ok_pre:.4}')
+        print(f'gap_ok_recall: {gap_ok_rec:.4}')
+        print(f'gap_ok_f1: {gap_ok_f1:.4}')
+        print(f'gap_bad_precision: {gap_bad_pre:.4}')
+        print(f'gap_bad_recall: {gap_bad_rec:.4}')
+        print(f'gap_bad_f1: {gap_bad_f1:.4}')
+        print(f'gap_mcc:{gap_mcc:.4}')
+        print('---')
+
+
+if __name__ == '__main__':
+    main()
--- evaluation-scripts/word_evaluation.py
+++ evaluation-scripts/word_evaluation.py
@@ -1,152 +1,85 @@
 #!/usr/bin/env python
-# -*- coding:utf-8 -*-
 
 import argparse
-import numpy as np
 import os
 
-from pathlib import Path
-from sklearn.metrics import f1_score, recall_score, precision_score
+from sklearn.metrics import precision_score, recall_score, f1_score
 
-
-def list_of_lists(a_list):
-    '''
-    check if <a_list> is a list of lists
-    '''
-    if isinstance(a_list, (list, tuple, np.ndarray)) and \
-            len(a_list) > 0 and \
-            all([isinstance(l, (list, tuple, np.ndarray)) for l in a_list]):
-        return True
-    return False
-
-
-def flatten(lofl):
-    '''
-    convert list of lists into a flat list
-    '''
-    if list_of_lists(lofl):
-        return [item for sublist in lofl for item in sublist]
-    elif type(lofl) == dict:
-        return lofl.values()
-
-
-def compute_scores(true_tags, test_tags):
-    flat_true = flatten(true_tags)
-    flat_pred = flatten(test_tags)
-    rev_flat_true = [1 if t == 0 else 0 for t in flat_true]
-    rev_flat_pred = [1 if t == 0 else 0 for t in flat_pred]
-
-    ok_preceision = precision_score(flat_true, flat_pred)
-    ok_recall = recall_score(flat_true, flat_pred)
-    bad_precision = precision_score(rev_flat_true, rev_flat_pred)
-    bad_recall = recall_score(rev_flat_true, rev_flat_pred)
-
-    bad_f1, ok_f1 = f1_score(flat_true, flat_pred, average=None, pos_label=None)
-
-    # Matthews correlation coefficient (MCC)
-    # true/false positives/negatives
-    tp = tn = fp = fn = 0
-    for pred_tag, gold_tag in zip(flat_pred, flat_true):
-        if pred_tag == 1:
-            if pred_tag == gold_tag:
-                tp += 1
-            else:
-                fp += 1
-        else:
-            if pred_tag == gold_tag:
-                tn += 1
-            else:
-                fn += 1
-
-    mcc_numerator = (tp * tn) - (fp * fn)
-    mcc_denominator = ((tp + fp) * (tp + fn) * (tn + fp) * (tn + fn)) ** 0.5
-    mcc = mcc_numerator / (mcc_denominator + 1e-5)
-
-    return ok_preceision, ok_recall, ok_f1, bad_precision, bad_recall, bad_f1, mcc
-
+PLAIN_TAGS = ['OK', 'BAD']
+FG_TAGS = ['OK', 'REP', 'INS', 'DEL']
+TAG_OPTIONS = {
+    'plain': PLAIN_TAGS,
+    'fine_grained': FG_TAGS
+}
 
 def parse_args():
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('-rst', '--ref_source_tags', type=Path)
-    parser.add_argument('-rt', '--ref_tags', type=Path, default=None)
-    parser.add_argument('-rgt', '--ref_gap_tags', type=Path, default=None)
+    parser.add_argument('-r', '--reference_prefix', required=True,
+                        help='Path prefix of reference files.')
+    parser.add_argument('-p', '--prediction_prefix', default='pred',
+                        help='Path prefix of prediciton files. Default: pred')
+    parser.add_argument('-m', '--mode', default='plain', choices=['fine_grained', 'plain'],
+                        help='Select an evaluation mode.\nAvailable: fine_grained, plain.\nDefault: plain')
 
-    parser.add_argument('-pst', '--pred_source_tags', type=Path)
-    parser.add_argument('-pt', '--pred_tags', type=Path, default=None)
-    parser.add_argument('-pgt', '--pred_gap_tags', type=Path, default=None)
+    return parser.parse_args()
 
-    # parser.add_argument('--evaluate-merged-mt', action='store_true',
-    #                     help='If this option is added, then MT word tags and gap tags will be evaluated together.')
+def check_file_exists(args):
+    assert os.path.isfile(args.reference_prefix + '.source_tags')
+    assert os.path.isfile(args.prediction_prefix + '.source_tags')
+    res = ['.source_tags']
+    if os.path.isfile(args.prediction_prefix + '.mtword_tags'):
+        assert os.path.isfile(args.reference_prefix + '.mtword_tags')
+        res.append('.mtword_tags')
+    if os.path.isfile(args.prediction_prefix + '.tags'):
+        assert os.path.isfile(args.prediction_prefix + '.tags')
+        res.append('.tags')
+    return res
 
-    args = parser.parse_args()
 
-    return args
+def compute_scores(refe_fn, pred_fn, tag_opts):
+    res = {}
+    ref_tags, pred_tags = [], []
+    with open(refe_fn) as f:
+        for line in f:
+            ref_tags.extend(line.strip().split())
+    with open(pred_fn) as f:
+        for line in f:
+            pred_tags.extend(line.strip().split())
 
+    for tag in tag_opts:
+        num_ref_tags = [1 if t == tag else 0 for t in ref_tags]
+        num_pred_tags = [1 if t == tag else 0 for t in pred_tags]
+        precision = precision_score(num_ref_tags, num_pred_tags)
+        recall = recall_score(num_ref_tags, num_pred_tags)
+        # f1 = f1_score(num_ref_tags, num_pred_tags, average='weighted', pos_label=None)
+        if precision + recall == 0:
+            f1 = 0
+        else:
+            f1 = 2 * (precision * recall) / (precision + recall)
+        res[tag] = {'precision': precision, 'recall': recall, 'f1': f1}
+    # res['total_f1'] = f1_score(ref_tags, pred_tags, average='weighted', pos_label=None)
+
+    return res
 
 def main():
     args = parse_args()
+    flags = check_file_exists(args)
 
-    TAG_MAP = {
-        'OK': 1,
-        'BAD': 0
-    }
+    for type in flags:
+        refe_fn = f'{args.reference_prefix}{type}'
+        pred_fn = f'{args.prediction_prefix}{type}'
+        tag_opts = TAG_OPTIONS[args.mode]
+        res = compute_scores(refe_fn, pred_fn, tag_opts)
 
-    def read_tag(fn):
-        with Path(fn).open() as f:
-            return [[TAG_MAP[t] for t in l.strip().split()] for l in f]
-
-    # Source
-    ref_source_tags = read_tag(args.ref_source_tags)
-    pred_source_tags = read_tag(args.pred_source_tags)
-    src_ok_pre, src_ok_rec, src_ok_f1, src_bad_pre, src_bad_rec, src_bad_f1, src_mcc = compute_scores(ref_source_tags,
-                                                                                                      pred_source_tags)
-    # f1_bad_src, f1_good_src, mcc_src = compute_scores(ref_source_tags, pred_source_tags)
-    src_f1_multi = src_ok_f1 * src_bad_f1
-
-    print(f'src_ok_precision: {src_ok_pre:.4}')
-    print(f'src_ok_recall: {src_ok_rec:.4}')
-    print(f'src_ok_f1: {src_ok_f1:.4}')
-    print(f'src_bad_precision: {src_bad_pre:.4}')
-    print(f'src_bad_recall: {src_bad_rec:.4}')
-    print(f'src_bad_f1: {src_bad_f1:.4}')
-    print(f'src_mcc:{src_mcc:.4}')
-    print('---')
-
-    if args.ref_tags is not None and args.pred_tags is not None:
-        # MT (only word or word&gap)
-        ref_mt_tags = read_tag(args.ref_tags)
-        pred_mt_tags = read_tag(args.pred_tags)
-        mt_ok_pre, mt_ok_rec, mt_ok_f1, mt_bad_pre, mt_bad_rec, mt_bad_f1, mt_mcc = compute_scores(ref_mt_tags,
-                                                                                                   pred_mt_tags)
-        mt_f1_multi = mt_ok_f1 * mt_bad_f1
-
-        print(f'mt_ok_precision: {mt_ok_pre:.4}')
-        print(f'mt_ok_recall: {mt_ok_rec:.4}')
-        print(f'mt_ok_f1: {mt_ok_f1:.4}')
-        print(f'mt_bad_precision: {mt_bad_pre:.4}')
-        print(f'mt_bad_recall: {mt_bad_rec:.4}')
-        print(f'mt_bad_f1: {mt_bad_f1:.4}')
-        print(f'mt_mcc:{mt_mcc:.4}')
-        print('---')
-
-    # GAP
-    if args.ref_gap_tags is not None and args.pred_gap_tags is not None:
-        ref_gap_tags = read_tag(args.ref_gap_tags)
-        pred_gap_tags = read_tag(args.pred_gap_tags)
-        gap_ok_pre, gap_ok_rec, gap_ok_f1, gap_bad_pre, gap_bad_rec, gap_bad_f1, gap_mcc = compute_scores(ref_gap_tags,
-                                                                                                          pred_gap_tags)
-        gap_f1_multi = gap_ok_f1 * gap_bad_f1
-
-        print(f'gap_ok_precision: {gap_ok_pre:.4}')
-        print(f'gap_ok_recall: {gap_ok_rec:.4}')
-        print(f'gap_ok_f1: {gap_ok_f1:.4}')
-        print(f'gap_bad_precision: {gap_bad_pre:.4}')
-        print(f'gap_bad_recall: {gap_bad_rec:.4}')
-        print(f'gap_bad_f1: {gap_bad_f1:.4}')
-        print(f'gap_mcc:{gap_mcc:.4}')
-        print('---')
-
+        print(f'======== {type}(P/R/F1) =========')
+        for tag in res:
+            info = res[tag]
+            print('{}: {:.4} / {:.4} / {:.4}'.format(
+                tag, info['precision'], info['recall'], float(info['f1'])
+            ))
+        # print('TOTAL F1: {}'.format(res['total_f1']))
+        print('')
 
 if __name__ == '__main__':
     main()
